798054f00fc3b3300484957b766bb20f5f6b3520
Marcos Pereira <marcospereiradev@gmail.com>
2021-04-02 22:15:34 +0100

    Update questions and style

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 const questions = [
   {
     question: "Are you going to sleep soon?",
-    points: 5,
+    points: 10,
     yesIsPositive: false,
     tips: [
       "You should ensure there will be enough time for the caffeine to wear out before trying to sleep.",
@@ -9,12 +9,66 @@ const questions = [
     ]
   },
   {
+    question: "Are you well rested but lacking motivation?",
+    points: 20,
+    yesIsPositive: true,
+    tips: [
+      "Coffee can be a great way of feeling excited about your work.",
+      "If you are well rested, it is unlikely to exacerbate stress and give you a bad time."
+    ]
+  },
+  {
+    question: "Do you think coffee isn't that good for you but still enjoy having it?",
+    points: 5,
+    yesIsPositive: false,
+    tips: [
+      "If you like the experience of having coffee but are suspicious of its effects, consider switching to decaffeinated coffee.",
+      "Consider having someone prepare your coffee randomly as either decaffeinated or not for a week. Note down each time whether you thought you were feeling the effects of caffeine, then check against the real data at the end of the week. You may confirm that coffee makes you feel bad, or disprove the hypothesis!"
+    ]
+  },
+  {
+    question: "Are you trying to feel better after having a bad night's sleep?",
+    points: 6,
+    yesIsPositive: false,
+    tips: [
+      "Having coffee without being well rested may be counterproductive.",
+      "You should consider taking a day off, if possible, and prioritize being well rested tomorrow.",
+      "You can be more productive in the long run by taking it easy when you need to."
+    ]
+  },
+  {
+    question: "Are you feeling stressed or anxious?",
+    points: 7,
+    yesIsPositive: false,
+    tips: [
+      "Coffee can exacerbate stress and anxiety. Consider having a cup of decaffeinated coffee or herbal tea instead."
+    ]
+  },
+  {
     question: "Are you trying to get through mindless work?",
-    points: 10,
+    points: 3,
     yesIsPositive: true,
     tips: [
       "Coffee is good at getting you into a mindset of getting work done.",
       "It might not be as good at providing a creative headspace."
+    ]
+  },
+  {
+    question: "Are you feeling withdrawal symptoms from suddenly stopping coffee?",
+    points: 5,
+    yesIsPositive: false,
+    tips: [
+      "Consider having one less coffee today than you did yesterday.",
+      "It may be a good idea to have a no-coffee day every few days to keep your tolerance low.",
+      "You can still have herbal tea or caffeine free coffee on no-coffee days!"
+    ]
+  },
+  {
+    question: "Do you enjoy coffee?",
+    points: 15,
+    yesIsPositive: true,
+    tips: [
+      "Doing what makes you happy is great. And you are great! Especially when you do the things you enjoy doing."
     ]
   }
 ];
